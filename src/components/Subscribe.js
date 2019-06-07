150e10a1a2a1d90b47d025fe36fc9fea3361359f
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-06-07 08:04:53 -0700

    Mailchimp integration test

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -14,7 +14,25 @@ class Subscribe extends Component {
     e.preventDefault();
 
     if (this.state.email) {
-      //TODO add email integration
+      const authString = btoa(`anystring:${process.env.API_KEY}`);
+      console.log(authString);
+      fetch('https://us20.api.mailchimp.com/3.0/lists/fb3a49a977/members/', {
+        method: 'POST',
+        mode: 'no-cors',
+        cach: 'no-cache',
+        credentials: "same-origin",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Basic ' + authString
+        },
+        redirect: 'follow',
+        referrer: 'no-referrer',
+        body: JSON.stringify({
+          email_address: this.state.email,
+          status: 'subscribed'
+        }),
+      }).then(data => console.log(data)).catch(error => console.log(`Error: ${error}`));
+
       this.setState({ email: "" });
     }
   };
@@ -26,6 +44,7 @@ class Subscribe extends Component {
       <form className="subscribe" onSubmit={e => this.onSubmit(e)}>
         <input
           className="subscribe-email"
+          name="email"
           type="email"
           placeholder={placeholder}
           onChange={e => this.onChange(e)}
