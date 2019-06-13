fed916c511e43a9d940a28de7cea143a49eed36b
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-06-13 16:20:34 -0700

    Toast messages

--- client/src/components/Subscribe.js
+++ client/src/components/Subscribe.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import { ToastContainer, toast } from 'react-toastify';
 import "../css/Subscribe.css";
+import "react-toastify/dist/ReactToastify.css"
 
 class Subscribe extends Component {
   state = {
-    email: ""
+    email: "",
+    message: ""
   };
 
   handleChange = e => {
@@ -15,16 +18,37 @@ class Subscribe extends Component {
 
     if (this.state.email) {
       fetch(`/api/memberAdd?email=${this.state.email}`)
-        .then()
+        .then(message => {
+          return message.json();
+        })
+        .then(json => {
+          if (json.status !== 'subscribed') {
+            this.setState({
+              message: "There was a problem with your email submission. Please check the email and try again."
+            });
+            this.error();
+          } else {
+            this.setState({
+              message: 'Thank you for subscribing to our mailing list. You will be receiving a welcome email shortly.'
+            });
+            this.success();
+          }
+        })
         .catch(err => {
           console.log("error", err);
         });
+
       this.props.toggleLogoSpeed();
-      setTimeout(this.props.toggleLogoSpeed, 2000);
+
+      setTimeout(this.props.toggleLogoSpeed, 1000);
+
       this.setState({ email: "" });
     }
-
   };
+
+  success = () => toast.success(this.state.message);
+
+  error = () => toast.error(this.state.message);
 
   render() {
     const { placeholder, buttonText } = this.props;
@@ -39,9 +63,16 @@ class Subscribe extends Component {
           onChange={this.handleChange}
           value={this.state.email}
         />
-        <button className="subscribe-button" type="submit" >
+        <button className="subscribe-button" type="submit">
           {buttonText}
         </button>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={true}
+          closeOnClick
+          pauseOnHover
+         />
       </form>
     );
   }
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const Mailchimp = require('mailchimp-api-v3');
-require('dotenv').config({ path: __dirname + '/variables.env'});
+const express = require("express");
+const Mailchimp = require("mailchimp-api-v3");
+require("dotenv").config({ path: __dirname + "/variables.env" });
 
 var mc_api_key = process.env.MAILCHIMP_API_KEY;
 var list_id = process.env.MAILCHIMP_LIST_ID;
@@ -9,20 +9,21 @@ const app = express();
 const mailchimp = new Mailchimp(mc_api_key);
 
 //routes
-app.get('/api/memberAdd', (req, res) => {
-    mailchimp.post(`/lists/${list_id}/members/`, {
-        email_address: req.query.email,
-        status: 'subscribed'
+app.get("/api/memberAdd", (req, res) => {
+  mailchimp
+    .post(`/lists/${list_id}/members/`, {
+      email_address: req.query.email,
+      status: "subscribed"
     })
-    .then( results => {
-        res.send(results);
+    .then(result => {
+      res.send(result);
     })
-    .catch( err => {
-        res.send(err);
+    .catch(err => {
+      res.send(err);
     });
 });
 
 const port = process.env.PORT || 9001;
 app.listen(port);
 
-console.log(`express app listening on port ${port}`);
+console.log(`Express listening on port ${port}`);
