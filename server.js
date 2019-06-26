fd9debdaf0e1b11f5f35ec2f70fb1c4623869eca
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-06-26 10:21:42 -0700

    Serving React app through Express

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const Mailchimp = require("mailchimp-api-v3");
 require("dotenv").config({ path: __dirname + "/variables.env" });
 
@@ -8,7 +9,10 @@ var list_id = process.env.MAILCHIMP_LIST_ID;
 const app = express();
 const mailchimp = new Mailchimp(mc_api_key);
 
-//routes
+// Serve static files from React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// API endpoints
 app.get("/api/memberAdd", (req, res) => {
   mailchimp
     .post(`/lists/${list_id}/members/`, {
