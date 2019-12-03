d322460261a4f135fcfa23371ba62adcd70f12d9
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-12-02 16:48:46 -0800

    changing notification to functional component

--- client/src/components/Notification.js
+++ client/src/components/Notification.js
@@ -1,20 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "../styles/Notification.css";
 
-class Notification extends Component {
-  render() {
-    return (
-      <div
-        className={`notification ${this.props.level}${
-          this.props.visible ? " visible" : ""
-        }`}
-      >
-        <img src={this.props.src} alt={this.props.alt} />
-        <p>{this.props.message}</p>
-      </div>
-    );
-  }
-}
+const Notification = ({ level, visible, src, alt, message }) => (
+  <div className={`notification ${level}${visible ? " visible" : ""}`}>
+    <img src={src} alt={alt} />
+    <p>{message}</p>
+  </div>
+);
 
 export default Notification;
